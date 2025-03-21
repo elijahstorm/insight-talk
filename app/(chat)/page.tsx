e49247a5d09531a7c03b72c72e3b5a997407bcf3
elijahstorm <elijahstormai@gmail.com>
2025-03-21 01:15:59 -0500

    setup front page

--- app/(chat)/page.tsx
+++ app/(chat)/page.tsx
@@ -1,28 +1,17 @@
-import { cookies } from 'next/headers'
-
-import { generateUUID } from '@/lib/utils'
-import Link from 'next/link'
+import { Overview } from '@/components/overview'
 
 export default async function Page() {
-	const id = generateUUID()
+	const history = []
 
-	const cookieStore = await cookies()
-	const modelIdFromCookie = cookieStore.get('chat-model')
+	if (history.length === 0) {
+		return <Overview />
+	}
 
 	return (
 		<div className="rounded-xl p-6 flex flex-col gap-8 leading-relaxed text-center max-w-xl">
-			<h2 className="text-xl font-bold">Talk Insight</h2>
-			<p>
-				You can learn more about how to use the Talk Insight AI by visiting the{' '}
-				<Link
-					className="font-medium underline underline-offset-4"
-					href="https://insight-talk.vercel.app/about"
-					target="_blank"
-				>
-					about
-				</Link>{' '}
-				page.
-			</p>
+			<input className="rounded-md bg-slate-100 text-slate-600 overflow-hidden border border-slate-200" />
+
+			<div>history</div>
 		</div>
 	)
 }
