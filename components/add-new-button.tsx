33f6280c8f7d07827d51f28f3ba96d4d95c2772c
elijahstorm <elijahstormai@gmail.com>
2025-03-21 07:39:27 -0500

    create AddNew mobile buttons

---
+++ components/add-new-button.tsx
@@ -0,0 +1,86 @@
+'use client'
+
+import { useRouter } from 'next/navigation'
+import React, { useState } from 'react'
+
+function HeadsUpButton({
+	action,
+	icon,
+	description,
+	hidden = false,
+}: {
+	action: () => void
+	icon: string
+	description: string
+	hidden?: boolean
+}) {
+	return (
+		<div
+			className={`flex gap-6 items-center justify-end transition-opacity ${hidden ? 'opacity-0' : 'pointer-events-auto'}`}
+		>
+			<p className="text-sm text-slate-600 pointer-events-none">{description}</p>
+
+			<button
+				className="w-14 h-14 bg-blue-500 text-white p-4 rounded-full shadow-lg hover:bg-blue-600 focus:outline-none focus:ring-2 focus:ring-blue-400 transition-colors"
+				onClick={action}
+			>
+				{icon}
+			</button>
+		</div>
+	)
+}
+
+export default function AddNewButton() {
+	const [menuClosed, setMenuClosed] = useState(true)
+	const router = useRouter()
+
+	const openMenu = () => {
+		setMenuClosed(false)
+	}
+
+	const closeMenu = () => {
+		setMenuClosed(true)
+	}
+
+	const startConvo = () => {
+		router.push('/chat/new')
+	}
+
+	const attachFile = () => {}
+
+	const attachImage = () => {}
+
+	return (
+		<div className="select-none">
+			<button
+				className={`fixed cursor-default inset-0 transition-opacity bg-white/30 backdrop-blur-sm ${menuClosed ? 'opacity-0 pointer-events-none' : ''}`}
+				onClick={closeMenu}
+			></button>
+
+			<div className="bottom-6 right-6 fixed">
+				<HeadsUpButton action={openMenu} description="" icon="+" />
+			</div>
+
+			<div className="bottom-6 right-6 fixed flex gap-8 flex-col-reverse pointer-events-none">
+				<HeadsUpButton
+					action={startConvo}
+					description="New Converstation"
+					icon="chat"
+					hidden={menuClosed}
+				/>
+				<HeadsUpButton
+					action={attachFile}
+					description="Attach a File"
+					icon="📎"
+					hidden={menuClosed}
+				/>
+				<HeadsUpButton
+					action={attachImage}
+					description="Attach an Image"
+					icon="🖼️"
+					hidden={menuClosed}
+				/>
+			</div>
+		</div>
+	)
+}
--- components/history.tsx
+++ components/history.tsx
@@ -15,19 +15,21 @@ type GroupedChats = {
 
 export default function History({ history }: { history: Chat[] }) {
 	const [filter, setFilter] = useState('')
+	const [typeFilter, setTypeFilter] = useState<'none' | keyof typeof typeEmojis>('none')
 
-	const filteredHistory = history.filter(
-		(chat) =>
-			chat.title.toLowerCase().includes(filter.toLowerCase()) ||
-			chat.summary.toLowerCase().includes(filter.toLowerCase())
-	)
-
-	const colorNumbers = ['50', '100', '200', '300', '400', '500', '600', '700', '800', '900', '950']
 	const typeEmojis = {
 		personal: '👫',
 		workplace: '💼',
 		legal: '⚖️',
 	} as const
+
+	const filteredByType =
+		typeFilter === 'none' ? history : history.filter((chat) => chat.type === typeFilter)
+	const filteredHistory = filteredByType.filter(
+		(chat) =>
+			chat.title.toLowerCase().includes(filter.toLowerCase()) ||
+			chat.summary.toLowerCase().includes(filter.toLowerCase())
+	)
 
 	const groupChatsByDate = (chats: Chat[]): GroupedChats => {
 		const now = new Date()
@@ -62,43 +64,66 @@ export default function History({ history }: { history: Chat[] }) {
 		)
 	}
 
-	const groupedChats = groupChatsByDate(history)
+	const groupedChats = groupChatsByDate(filteredHistory)
+
+	const timeGroupedText = {
+		today: 'Today',
+		yesterday: 'Yesterday',
+		lastWeek: 'Last Week',
+		lastMonth: 'Last Month',
+		older: 'Older',
+	}
 
 	return (
-		<div className="flex flex-col gap-8">
-			<input
-				type="text"
-				value={filter}
-				onChange={(e) => setFilter(e.target.value)}
-				className="rounded-md bg-slate-100 text-slate-600 overflow-hidden border border-slate-200 py-2 px-3"
-				placeholder="Search Conversations"
-			/>
+		<div className="flex flex-col gap-8 text-left">
+			<div className="flex gap-4 ">
+				<input
+					type="text"
+					value={filter}
+					onChange={(e) => setFilter(e.target.value)}
+					className="rounded-md bg-slate-50 text-slate-800 overflow-hidden border border-slate-200 py-2 px-3 w-full"
+					placeholder="Search Conversations"
+				/>
 
-			<div className="grid grid-cols-2 gap-4">
-				{filteredHistory.map((chat, index) => (
-					<Link
-						key={chat.id}
-						className={`relative text-slate-800 p-4 rounded-md text-left bg-graphcolor-${colorNumbers[index % colorNumbers.length]}`}
-						href={`/chat/${chat.id}`}
-					>
-						<div className="w-full flex items-center gap-2">
-							<h2
-								className={`font-semibold flex-1 text-sm text-graphtextcolor-${colorNumbers[index % colorNumbers.length]} line-clamp-1`}
-							>
-								{chat.title}
-							</h2>
-							<div className="pl-1 pt-0.5 text-sm rounded-full h-6 w-6 bg-slate-100 border border-slate-500">
-								{typeEmojis[chat.type]}
-							</div>
-						</div>
-						<p
-							className={`text-sm text-graphtextcolor-${colorNumbers[index % colorNumbers.length]}/50 line-clamp-2`}
-						>
-							{chat.summary}
-						</p>
-					</Link>
-				))}
+				<select
+					value={typeFilter}
+					onChange={(e) => setTypeFilter(e.target.value as 'none' | keyof typeof typeEmojis)}
+					className="rounded-md bg-slate-100 text-slate-600 border border-slate-200 py-2 px-3  text-xs"
+				>
+					<option value="none">All Types</option>
+					{Object.keys(typeEmojis).map((type) => (
+						<option key={type} value={type}>
+							{type.charAt(0).toUpperCase() + type.slice(1)}
+						</option>
+					))}
+				</select>
 			</div>
+
+			{(Object.keys(groupedChats) as Array<keyof GroupedChats>).map((timeGroup) =>
+				groupedChats[timeGroup].length ? (
+					<div key={timeGroup} className="space-y-2">
+						<h1 className="text-slate-600 text-lg font-bold">{timeGroupedText[timeGroup]}</h1>
+
+						<div className="grid grid-cols-2 gap-4">
+							{groupedChats[timeGroup].map((chat) => (
+								<Link
+									key={chat.id}
+									className="border text-slate-800 p-4 rounded-md bg"
+									href={`/chat/${chat.id}`}
+								>
+									<div className="w-full flex items-center gap-2">
+										<h2 className="font-semibold flex-1 text-sm line-clamp-1">{chat.title}</h2>
+										<div className="pl-1 pt-0.5 text-sm rounded-full h-6 w-6 bg-slate-100 border border-slate-500">
+											{typeEmojis[chat.type as keyof typeof typeEmojis]}
+										</div>
+									</div>
+									<p className="text-sm line-clamp-2">{chat.summary}</p>
+								</Link>
+							))}
+						</div>
+					</div>
+				) : null
+			)}
 		</div>
 	)
 }
